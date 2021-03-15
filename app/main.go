3072fdbd85f91288e677532c2b1156606d1d9016
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2021-03-15 21:40:49 +0700

    Update main.go

--- app/main.go
+++ app/main.go
@@ -18,7 +18,7 @@ func main() {
 	topic := "message"
 	partition := 0
 
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka:9092", topic, partition)
 	if err != nil {
 		log.Fatal("failed to dial leader:", err)
 	}
